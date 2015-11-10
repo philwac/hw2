1d94b089c528c0b401565fbd65fd485372217689
Philipp Wachenfeld <PhilWac@linux-ywbc.site>
2015-11-10 18:13:11 +0100

    Homework 2

--- logmap.cxx
+++ logmap.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -6,18 +7,27 @@ int main(){
 	double x0 = 0.5;
 	double x;
 	int Nskip = 100; //Number of iterations to skip
-	int Nend  = 200; //Number of total iterations
+	int Nend = 200; //Number of total iterations
+	int deltaN = Nend-Nskip;
+	double results[deltaN];
+
+	ofstream out("output.dat");
 
 	for(double r=0; r <= 4; r += 0.001){
-	   x=x0;
-	   for(int i=1; i <= Nskip; i++)
-		   x = r*x*(1-x);
-	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
-   	   }
+	    x=x0;
+	    for(int i=1; i <= Nskip; i++)
+	         x = r*x*(1-x);
+	    for(int i=Nskip+1; i <= Nend; i++){
+	         x = r*x*(1-x);
+	         results[i-Nskip-1] = x;
 	}
 
+	for (int i=0; i<= deltaN; i++) {
+	     out << r << "\t" << results[i] << endl;
+	}
 
+	}
+	
+	out.close();
 	return 0;
 }
